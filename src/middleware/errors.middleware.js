b1f8bf7c40a56f1942d7dc8c470a97b315587742
Duaazayed <dzayed@regis.edu>
2022-03-26 20:13:03 -0600

    initial commit :wq

---
+++ src/middleware/errors.middleware.js
@@ -0,0 +1,16 @@
+exports.error404 = function(req, res, next){
+    next({message: 'Not Found', status: 404});
+};
+exports.error500 = function(error, req, res, next){
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message: error.message
+        }
+    });
+};
+exports.logger = function(port){
+    return function(){
+        console.log(`Running on port: ${port}...`);
+    }
+}
